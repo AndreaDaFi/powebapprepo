194c448dd9f41e8e63dbcda705f1f248c0e083fb
AndreaDaFi <adavila9@cougarnet.uh.edu>
2025-03-07 22:50:10 -0600

    my beautiful empty webapp is back

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Contact from './pages/Contact'
-import About from './pages/About'
-import NoPage from './pages/NoPage'
+import logo from './logo.svg';
+import './App.css';
 
-export default function App(){
-  return(
-    <div>
-        <BrowserRouter>
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<NoPage />} />
-          </Routes>
-        </BrowserRouter>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
+
+export default App;
--- src/components/Header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Header(){
-    return(
-        <h1>header created by following tutorial</h1>
-    )
-}
