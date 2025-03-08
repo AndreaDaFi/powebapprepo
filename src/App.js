e66cf2454933a46ffe50786c6e066946c91e497d
AndreaDaFi <adavila9@cougarnet.uh.edu>
2025-03-07 22:47:43 -0600

    deleting everything

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './navbar';  // Make sure the path is correct
-import Customerlogin from './customerlogin';
-import Employeelogin from './employeelogin';
-import Home from './home';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import NoPage from './pages/NoPage'
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Navbar />  {/* Include the Navbar here */}
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/employeelogin" element={<Employeelogin />} />
-          <Route exact path="/customerlogin" element={<Customerlogin />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+export default function App(){
+  return(
+    <div>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<NoPage />} />
+          </Routes>
+        </BrowserRouter>
+    </div>
+  )
 }
-
-export default App;
---
+++ src/pages/NoPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/Header'
+
+export default function NoPage(){
+    return(
+        <div>
+            <Header />
+            <h2>Error 404: Not fount</h2>
+        </div>
+    )
+}
